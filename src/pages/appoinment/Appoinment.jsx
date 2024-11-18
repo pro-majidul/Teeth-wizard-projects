fe2325a3f298800e6c12d7277ae61fd58f8065c9
Majidul Islam <160348093+pro-majidul@users.noreply.github.com>
2024-11-18 06:47:35 +0600

    all work done

--- src/pages/appoinment/Appoinment.jsx
+++ src/pages/appoinment/Appoinment.jsx
@@ -1,15 +1,33 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthProvider } from "../../provider/Provider";
 
 const Appoinment = () => {
-    const {user } = useContext(AuthProvider);
-    console.log(user);
-    
+    const { user } = useContext(AuthProvider);
+    const [savedata, setSavedata] = useState([])
+
+
+
+    useEffect(() => {
+        const storageItems = localStorage.getItem('appoinment');
+        const data = JSON.parse(storageItems);
+        const items = data.filter(item => item.email === user.email);
+        setSavedata(items)
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+
+
+
+
 
 
     return (
         <div>
-            appoinments 
+           {
+            savedata.map( (item, i) => <div key={i}>
+                <p>{item.email}</p>
+            </div>)
+           }
         </div>
     );
 };
